db31a0787492637fca70934a6e02f7cdcb55e408
KatharineME <katharine.me@icloud.com>
2021-01-19 10:41:57 -0800

    cleaning functions

---
+++ src/tabix_regions_from_file.jl
@@ -0,0 +1,19 @@
+include("print_and_run_cmd.jl")
+
+function tabix_regions_from_file(
+        bed_file_path::String, 
+        vcf_file_path::String,
+        output_dir::String
+    )
+   
+    bed_split_file_path = Array(split(bed_file_path, "/"))
+
+    bed_name = split(bed_split_file_path[size(bed_split_file_path)[1]], ".")[1]
+    
+    output_file_path = joinpath(output_dir, bed_name)
+    
+    VCFTool.print_and_run_cmd(`tabix -R $bed_file_path $vcf_file_path`, output_file_path)
+    
+end
+
+export tabix_regions_from_file
