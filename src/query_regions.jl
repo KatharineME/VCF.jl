4735221e573b9b1cb3f4b53c18191e06e9f8ea5b
KatharineME <katharine.me@icloud.com>
2021-01-21 15:45:01 -0800

    query function

---
+++ src/query_regions.jl
@@ -0,0 +1,44 @@
+using CSV: File
+
+using DataFrames
+
+using Dates
+
+using Query 
+
+
+function query_regions(
+    regions::Dict,
+    vcf::DataFrame,
+    output_dir::String,
+    )
+    
+    start_time = now()
+    
+    for (key, value) in regions
+
+        println(key)
+
+        println(value)
+
+        query = @from i in vcf begin
+            @where i.CHROM==key
+            @where i.POS==value
+            @select {i.CHROM, i.POS, i.REF, i.ALT, i.FORMAT, i.Germ}
+            @collect DataFrame
+
+        end
+
+        println("\n")
+
+        println(query)
+
+    end
+
+    end_time = now()
+
+    println("\nTook $(canonicalize(Dates.CompoundPeriod(end_time - start_time))).\n")
+    
+end
+
+export query_regions
