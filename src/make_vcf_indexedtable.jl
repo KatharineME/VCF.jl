abbf3f5c9a22699c049d4814ca36b1048b2288fc
KatharineME <katharine.me@icloud.com>
2021-03-19 16:53:32 -0700

    vcf indextable seeing the light

--- src/make_vcf_indexedtable.jl
+++ src/make_vcf_indexedtable.jl
@@ -54,7 +54,7 @@ function make_vcf_indexedtable(vcf_gz_file_path)
 
     end
 
-    vcf_table = transform(vcf_table, :CHROM => chromosome_numeric)
+    vcf_table = transform(vcf_table, :CHROM => chromosomes_numeric)
 
 
     # Convert positions to numbers
