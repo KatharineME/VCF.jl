806dacf330f7c4e7e6a2da5f78e0ec78382313d9
KatharineME <katharine.me@icloud.com>
2021-03-24 17:31:32 -0700

    3

--- src/make_vcf_indexedtable.jl
+++ src/make_vcf_indexedtable.jl
@@ -25,61 +25,74 @@ function make_vcf_indexedtable(vcf_gz_file_path)
 
     end
     
-    vcf_table = loadtable(
-        vcf_file_path,
-        delim='\t',
-        header_exists=false, 
-        colnames=[:CHROM, :POS, :ID, :REF, :ALT, :QUAL, :FILTER, :INFO, :FORMAT, :GERM],
-        indexcols=[:CHROM],
-    )
-
-
-    # Remove header row
+    # Check for Dagger file to load
     
-    n_rows = length(rows(vcf_table))
-
-    vcf_table = vcf_table[2:n_rows]
+    vcf_dagger_file_path = vcf_file_path * ".dagger"
     
-
-    # Convert chromosome strings to chromosome numbers
-
-    chromosomes_string = Array(columns(vcf_table)[1])
-
-    chromosomes_numeric = []
-
-    for item in chromosomes_string
-
-        parts = split(item, "")
-
-        chromosome_number = parts[length(parts)]
-
-        push!(chromosomes_numeric, parse(Int, chromosome_number))
-
-    end
-
-    vcf_table = IndexedTables.transform(vcf_table, :CHROM => chromosomes_numeric)
-
-
-    # Convert positions to numbers
-
-    positions_string = Array(columns(vcf_table)[2])
-
-    positions_numeric = []
-
-    for item in positions_string
-
-        push!(positions_numeric, parse(Int, item))
+    if isfile(vcf_dagger_file_path) == true
         
+        vcf_table = load(vcf_dagger_file_path)
+   
+    # Create table if Dagger file doesn't exist
+        
+    else
+    
+        vcf_table = loadtable(
+            vcf_file_path,
+            delim='\t',
+            header_exists=false, 
+            colnames=[:CHROM, :POS, :ID, :REF, :ALT, :QUAL, :FILTER, :INFO, :FORMAT, :GERM],
+            indexcols=[:CHROM],
+        )
+
+        # Remove header row
+
+        n_rows = length(rows(vcf_table))
+
+        vcf_table = vcf_table[2:n_rows]
+
+        # Convert chromosome strings to chromosome numbers
+
+        chromosomes_string = Array(columns(vcf_table)[1])
+
+        chromosomes_numeric = []
+
+        for item in chromosomes_string
+
+            parts = split(item, "")
+
+            chromosome_number = parts[length(parts)]
+
+            push!(chromosomes_numeric, parse(Int, chromosome_number))
+
+        end
+
+        vcf_table = IndexedTables.transform(vcf_table, :CHROM => chromosomes_numeric)
+
+        # Convert positions to numbers
+
+        positions_string = Array(columns(vcf_table)[2])
+
+        positions_numeric = []
+
+        for item in positions_string
+
+            push!(positions_numeric, parse(Int, item))
+
+        end
+
+        vcf_table = IndexedTables.transform(vcf_table, :POS => positions_numeric)
+
+        # Reindex
+
+        vcf_table = reindex(vcf_table, (:CHROM, :POS))
+        
+        # Save Dagger file
+        
+        save(vcf_table, vcf_dagger_file_path)
+
     end
-
-    vcf_table = IndexedTables.transform(vcf_table, :POS => positions_numeric)
-
-
-    # Reindex
-
-    vcf_table = reindex(vcf_table, (:CHROM, :POS))
-
-
+    
     return vcf_table
 
     end_time = now()
@@ -90,4 +103,3 @@ end
 
 
 export make_vcf_indexedtable
-
